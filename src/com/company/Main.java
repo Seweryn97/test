9a3fec0c2c1ea51989d8b8c94b2c6996f4a25985
Seweryn <sewerynde@gmail.com>
2020-10-18 23:39:05 +0200

    Opis 4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import javax.sound.midi.Soundbank;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Cos");
+        System.out.println("   ");
+        System.out.println("Cos2");
     }
 }
